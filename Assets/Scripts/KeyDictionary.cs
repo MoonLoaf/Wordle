0b2fb243c4c7b4b126f43341559a287eb98ab25a
MoonLoaf <oskarw.99@hotmail.com>
2022-11-30 09:54:00 +0100

    Dictionary for keyboard added

---
+++ Assets/Scripts/KeyDictionary.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KeyDictionary : MonoBehaviour
+{
+    public Dictionary<char, GameObject> KeyboardDict;
+
+    private void Start()
+    {
+        KeyboardDict = new Dictionary<char, GameObject>();
+
+        AddDescendants(transform, KeyboardDict);
+        
+        Debug.Log(KeyboardDict.ContainsKey('Q'));
+    }
+    private void AddDescendants(Transform parent, Dictionary<char, GameObject> dict)
+    {
+        foreach (Transform child in parent)
+        {
+            dict.Add(child.GetComponentInChildren<TMP_Text>().text[0], child.gameObject);
+        }
+    }
+}
